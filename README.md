057886a0fbddba0404326b43f77f4a854be3fd23
Bob Wombat Hogg <wombat@rwhogg.site>
2020-04-26 10:20:36 -0400

    Groovy

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Hi
+# cvelookup
+
+Simple CLI utility to look up CVE details from the NVD.
+
+## Note
+
+This tool was not made or endorsed by the NVD, MITRE, or any similar or affiliated organization.
+There is no guarantee it will work on arbitrary CVE's, nor that it will return complete data.
+(In fact, by design, it does not).
---
+++ src/main/groovy/cvelookup/App.groovy
@@ -0,0 +1,43 @@
+package cvelookup
+
+import groovy.json.JsonSlurper
+
+class App
+{
+    static void main(String[] cliArgs)
+    {
+        def cli = new CliBuilder(usage: "cvelookup <CVE ID>")
+        cli.help("Print this message")
+        def options = cli.parse(cliArgs)
+        def args = options.arguments() 
+        if(args == null || args.length == 0 || args[0] == null)
+        {
+            cli.usage()
+            System.exit(0)
+        }
+
+        def cveId = args[0]
+        def cveBase = "https://services.nvd.nist.gov/rest/json/cve/1.0/"
+        def url = new URL(cveBase + cveId)
+        def results = url.text
+        def jsonSlurper = new JsonSlurper()
+        def cveData = jsonSlurper.parseText(results)["result"]["CVE_Items"][0]["cve"]
+
+        def description = cveData["description"]["description_data"][0]["value"]
+        def cwe = cveData["problemtype"]["problemtype_data"][0]["description"]["value"][0]
+        def references = cveData["references"]["reference_data"]
+
+        println(cveId)
+        println("CWE: " + cwe)
+        println(description)
+        println("References:")
+        for(def reference: references)
+        {
+            println("\t" + reference["url"])
+        }
+
+        // Temp debug
+        println()
+        println(cveData)
+    }
+}
